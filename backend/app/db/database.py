964510b7daf7e6ca6cba17d150c5001250c017d7
Dennis Gera <geradennis@gmail.com>
2024-12-13 15:32:38 -0300

    feat: add commit

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -39,6 +39,12 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except:  # noqa: E722
+        db.rollback()
+        raise
+    else:
+        if db.is_active:
+            db.commit()
     finally:
         db.close()
 
